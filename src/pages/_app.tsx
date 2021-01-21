4317671351c580bb4bd52422e0ff725d80b83331
Andre Morpa <andremorpa@gmail.com>
2021-01-21 20:56:59 +0000

    Add web fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,46 @@
+import 'styles/fonts.css'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
+import { Router } from 'next/router'
+import NProgress from 'nprogress'
+import { AnimateSharedLayout } from 'framer-motion'
+
 import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
+
+NProgress.configure({ showSpinner: false })
+
+Router.events.on('routeChangeStart', () => {
+  NProgress.start()
+})
+
+Router.events.on('routeChangeComplete', () => {
+  NProgress.done()
+})
+
+Router.events.on('routeChangeError', () => {
+  NProgress.done()
+})
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>PokeNext</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="Your optimized Pokédex, in server side rendering."
         />
       </Head>
-      <GlobalStyles />
-      <Component {...pageProps} />
-    </>
+      <AnimateSharedLayout>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </AnimateSharedLayout>
+    </ThemeProvider>
   )
 }
 
---
+++ src/styles/theme.ts
@@ -0,0 +1,44 @@
+export default {
+  grid: {
+    container: '120rem',
+    gutter: '3.2rem'
+  },
+
+  border: {
+    radius: '0.4rem'
+  },
+
+  font: {
+    family:
+      "Lato, Abel, -apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans- serif",
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '2.8rem'
+    }
+  },
+
+  colors: {
+    yellow: '#FFDE00',
+    red: '#FF4040',
+    green: '#2EC4B6',
+    dark: '#030517',
+    grey: '#1B2329',
+    midGrey: '#353F4C',
+    lightGrey: '#7A8C99',
+    white: '#FAFAFA'
+  },
+
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem'
+  }
+} as const
