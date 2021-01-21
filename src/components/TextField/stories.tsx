3311e49e5829cf3c14aef6d95d7d11849d430039
Andre Morpa <andremorpa@gmail.com>
2021-01-21 22:08:21 +0000

    Add TextField component

---
+++ src/components/TextField/stories.tsx
@@ -0,0 +1,35 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { Search } from '@styled-icons/material-outlined'
+
+import TextField, { TextFieldProps } from '.'
+
+export default {
+  title: 'TextField',
+  component: TextField,
+  args: {
+    name: 'search',
+    icon: <Search />,
+    initialValue: '',
+    placeholder: 'Busque um filme por nome ou gênero...'
+  },
+  argTypes: {
+    onInput: { action: 'changed' },
+    icon: { type: '' }
+  }
+} as Meta
+
+export const Default: Story<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
+
+export const withError: Story<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 300, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
+
+withError.args = {
+  error: 'Ops...ocorreu um erro!'
+}
---
+++ src/components/TextField/styles.ts
@@ -0,0 +1,73 @@
+import styled, { css, DefaultTheme } from 'styled-components'
+
+import { TextFieldProps } from '.'
+
+type IconPositionProps = Pick<TextFieldProps, 'iconPosition'>
+
+type WrapperProps = Pick<TextFieldProps, 'disabled'> & { error?: boolean } & {
+  isLoading?: boolean
+}
+
+const wrapperModifiers = {
+  error: (theme: DefaultTheme) => css`
+    ${InputWrapper} {
+      border-color: ${theme.colors.red};
+    }
+    ${Icon}
+  `
+}
+
+export const InputWrapper = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    background: ${theme.colors.auxColor};
+    border-radius: 2rem;
+    padding: 0 ${theme.spacings.xsmall};
+    border: 0.2rem solid;
+    border-color: ${theme.colors.acqua};
+
+    &:focus-within {
+      box-shadow: 0 0 0.5rem ${theme.colors.acqua};
+    }
+  `}
+`
+
+export const Input = styled.input<IconPositionProps>`
+  ${({ theme, iconPosition }) => css`
+    color: ${theme.colors.background};
+    font-family: ${theme.font.family};
+    font-size: ${theme.font.sizes.medium};
+    padding: 1.2rem 0;
+    padding-${iconPosition}: ${theme.spacings.xsmall};
+    background: transparent;
+    border: 0;
+    outline: none;
+    width: 100%;
+  `}
+`
+
+export const Icon = styled.div<IconPositionProps>`
+  ${({ theme, iconPosition }) => css`
+    display: flex;
+    width: 2.2rem;
+    color: ${theme.colors.textColor};
+    order: ${iconPosition === 'right' ? 1 : 0};
+
+    & > svg {
+      width: 100%;
+    }
+  `}
+`
+
+export const Error = styled.span`
+  ${({ theme }) => css`
+    color: ${theme.colors.red};
+    font-size: ${theme.font.sizes.xsmall};
+  `}
+`
+
+export const Wrapper = styled.div<WrapperProps>`
+  ${({ theme, error }) => css`
+    ${error && wrapperModifiers.error(theme)};
+  `}
+`
